f66d5deb1fddc6ef4481d9fa8b5b3b917bd995e4
LuminEe <1123462990@qq.com>
2017-07-20 14:07:26 +0800

    make [getVersion] and [getAvailableVersion] compatible

--- src/Helper/Helper.php
+++ src/Helper/Helper.php
@@ -7,11 +7,36 @@ use Illuminate\Http\Request;
 
 class Helper
 {
-    public function getVersion(Request $request, Modules $modules, $router, $module_name, $namespase)
+    public function getVersion(Request $request, Modules $modules, $module_name)
     {
         $version  = $request->header('version');
         $versions = $modules->get($module_name.'::version');
-        
+        if (!in_array($version, $versions)) {
+            $r_versions = array_reverse($versions);
+            if (!$version) {
+                $version = $r_versions[0];
+            }else {
+                for ($i = 0; $i < count($r_versions); $i++) {
+                    if (version_compare($version, $r_versions[$i], '<')) {
+                        continue;
+                    }
+                    $version = $r_versions[$i];
+                    break;
+                }
+                if (!in_array($version, $versions)) {
+                    $version = $versions[0];
+                }
+            }
+        }
+        $version = str_replace('.', '_', $version);
+        return $version;
+    }
+
+    public function getAvailableVersion(Request $request, Modules $modules, $router, $module_name, $namespase)
+    {
+        $version  = $request->header('version');
+        $versions = $modules->get($module_name.'::version');
+
         $r_versions = array_reverse($versions);
         if (!$version) {
             $version = $r_versions[0];
@@ -33,7 +58,7 @@ class Helper
         $version = str_replace('.', '_', $version);
         return $version;
     }
-    
+
     public function checkActionExists($request, $router, $namespace)
     {
         try {
